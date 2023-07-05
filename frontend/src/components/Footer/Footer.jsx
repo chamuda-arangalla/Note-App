42036c62cd11c27aa3b11619c1830ed470a4bba4
IT21211850 <it21211850@my.sliit.lk>
2023-07-05 09:48:00 +0530

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer
+      className="bg-body-tertiary text-center text-lg-start"
+      style={{
+        width: "100%",
+        position: "relative",
+        bottom: "0",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col>About Us</Col>
+          <Col>Contact Us</Col>
+        </Row>
+        <Row>
+          <Col>We are the best</Col>
+          <Col>+9411 5688222</Col>
+        </Row>
+        <Row>
+          <Col>Copyright &copy; Note</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
